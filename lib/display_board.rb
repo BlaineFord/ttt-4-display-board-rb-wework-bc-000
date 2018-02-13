5d92ae75202154309b45739b55d27a1c25dda1d6
Blaine Ford <bford@wework.com>
2018-02-13 05:13:34 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,6 +2,12 @@
 # out the current state.
 
 board = [" "," "," "," "," "," "," "," "," "]
+midde_x = [" "," "," ","X"," "," "," "," "," "]
+all_x = ["X","X","X","X","X","X","X","X","X"]
+all_o = ["O","O","O","O","O","O","O","O","O"]
+o_top_left = ["O"," "," "," "," "," "," "," "," "]
+next_one = ["O"," "," "," ","X"," "," "," "," "]
+x_win = ["X","X","X"," "," "," "," "," "," "]
 
 def display_board (board)
   print
